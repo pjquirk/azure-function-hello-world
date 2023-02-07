b2d8c7a10cb5014ed07ac5331deed1c12e164b24
PJ Quirk <pjquirk@github.com>
2023-02-07 10:38:15 -0500

    Update response

--- function/HelloWorld.cs
+++ function/HelloWorld.cs
@@ -22,7 +22,7 @@ namespace pjquirk.demo
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            response.WriteString("Welcome to your private network!");
 
             return response;
         }
